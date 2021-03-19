6f675cd77fd111bd03eddaa27c17d3228f52e5f0
Psyh2409 <psyh2409@gmail.com.com>
2021-03-19 05:27:43 +0200

    short Bias and UnSynapse

--- src/main/java/com/gmail/psyh2409/artificialNeuralNetworkWithBackPropagation/Bias.java
+++ src/main/java/com/gmail/psyh2409/artificialNeuralNetworkWithBackPropagation/Bias.java
@@ -11,27 +11,9 @@ import java.util.List;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Bias extends Neuroneus implements Passer{
-    private List<Double> ins;
-    private double sum;
     private final double out = 1;
-    private List<Synapse> outs;
-
-    public Bias() {
-        ins = new ArrayList<>();
-        outs = new ArrayList<>();
-    }
-
-    private double summer(List<Double> ins) {
-        sum = ins.stream().reduce(0.0, (a, b) -> a + b);
-        return sum;
-    }
-
-    private double activationSigmoid(double in) {
-        return out;
-    }
-
+    @Override
     public double forwardLifeCircle() {
-        return out;
-
+        return 1;
     }
 }
--- src/main/java/com/gmail/psyh2409/artificialNeuralNetworkWithBackPropagation/UnSynapse.java
+++ src/main/java/com/gmail/psyh2409/artificialNeuralNetworkWithBackPropagation/UnSynapse.java
@@ -1,22 +1,17 @@
 package com.gmail.psyh2409.artificialNeuralNetworkWithBackPropagation;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
 public class UnSynapse extends Synapse implements Passer{
-    private Passer inNeuroneus;
-    private double justice;
-    private Passer outNeuroneus;
-    private double inDouble;
+    private final double justice = 0;
 
-    public UnSynapse() {
-        super();
-        justice = 0;
+    public UnSynapse(Neuroneus before, Neuroneus after) {
+        super(before, after);
     }
 
-    public UnSynapse(Passer inNeuroneus, Passer outNeuroneus) {
-        super();
-        justice = 0;
-        this.inNeuroneus = inNeuroneus;
-        this.outNeuroneus = outNeuroneus;
-    }
 
     public double weighting(double in){
         return in * justice * 0;
@@ -24,7 +19,6 @@ public class UnSynapse extends Synapse implements Passer{
 
     @Override
     public double forwardLifeCircle() {
-        inDouble = inNeuroneus.forwardLifeCircle();
-        return weighting(inDouble);
+        return weighting(0);
     }
 }
